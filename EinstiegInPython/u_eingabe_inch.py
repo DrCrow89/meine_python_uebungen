fed61dd65e041b0f98854105703a12522260c95f
Roland Kuenen <rolandkuenen@gmx.net>
2019-01-13 21:37:56 +0100

    Funktion erstellt, um Inch Werte in cm umzurechnen

---
+++ EinstiegInPython/u_eingabe_inch.py
@@ -0,0 +1,11 @@
+# Eingaben
+print("Bitte geben Sie den Inch-Wert ein:")
+eingabe = input()
+
+# Berechnung
+# 1-Inch sind 2.54cm
+inch_wert = float(eingabe)
+ergebnis = inch_wert*2.54
+
+# Ausgabe
+print inch_wert, "Inch sind", ergebnis, "cm"
