60b6de7f27b2d4d99a8a09c956964036d920e642
Roland Kuenen <rolandkuenen@gmx.net>
2019-01-13 21:50:45 +0100

    Programm erstellt

---
+++ EinstiegInPython/u_eingabe_gehalt.py
@@ -0,0 +1,10 @@
+# Eingaben
+print "Geben Sie Ihr Bruttogehalt in Euro ein:"
+eingabe = input()
+
+# Berechnung
+gehalt_wert = float(eingabe)
+ergebnis = gehalt_wert*0.18
+
+# Ausgabe
+print "Es ergibt sich ein Steuerbetrag von", ergebnis, "Euro"
