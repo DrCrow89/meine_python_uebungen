6f6baa4441315bc08792b7c1dfaaba42fb28edb7
Roland Kuenen <rolandkuenen@gmx.net>
2019-01-15 23:03:26 +0100

    Aufgabenstellung mit If-Verzweigung

---
+++ EinstiegInPython/u_verzweigung_einfach.py
@@ -0,0 +1,13 @@
+# Eingaben
+print "Geben Sie Ihr Bruttogehalt in Euro ein:"
+eingabe = input()
+
+# Berechnung
+gehalt_wert = float(eingabe)
+if gehalt_wert > 2500:
+    ergebnis = gehalt_wert*0.22
+else:
+    ergebnis = gehalt_wert*0.18
+
+# Ausgabe
+print "Es ergibt sich ein Steuerbetrag von", ergebnis, "Euro"
