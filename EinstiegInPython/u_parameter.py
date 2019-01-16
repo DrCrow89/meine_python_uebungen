2ffda0d399c01e809020ef72039f8875102d49ab
Roland Kuenen <rolandkuenen@gmx.net>
2019-01-16 16:09:54 +0100

    Aufgabenstellung erfüllt

--- EinstiegInPython/u_parameter.py
+++ EinstiegInPython/u_parameter.py
@@ -1,20 +1,12 @@
 def steuer(uebergabe_gehalt):
-    if gehalt_wert > 2500:
-        ergebnis = gehalt_wert*0.22
+    if uebergabe_gehalt > 2500:
+        ergebnis = uebergabe_gehalt*0.22
     else:
-        ergebnis = gehalt_wert*0.18
+        ergebnis = uebergabe_gehalt*0.18
     # Ausgabe
     print "Es ergibt sich ein Steuerbetrag von", ergebnis, "Euro"
 
-fehler = 1
-while fehler != 0:
-    try:
-        # Eingaben
-        print "Geben Sie Ihr Bruttogehalt in Euro ein:"
-        eingabe = input()
-        # Berechnung
-        gehalt_wert = float(eingabe)
-        steuer(gehalt_wert)
-        fehler = 0
-    except:
-        print "Der eingegebene Wert war keine Zahl"
+steuer(1800)
+steuer(2200)
+steuer(2500)
+steuer(2900)
