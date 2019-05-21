b897dfce22316af76886025e1cebaf001d490aac
DrCrow89 <rolandkuenen@gmx.net>
2019-05-21 06:34:54 +0200

    Funktion zur Primfaktorzerlegung in rkmath Modul verschoben.

--- Pythonbuchcom/pbc_a02_08.py
+++ Pythonbuchcom/pbc_a02_08.py
@@ -5,21 +5,13 @@ sys.path.insert(0, '/home/roland/git/meine_python_uebungen/')
 import allg_func, rkmath
 
 def main():
-    zer_liste = []
 
     while True:
         eingabe = int(allg_func.get_eingabe_float("Bitte eine natürliche Zahl für die Primfaktorzerlegung eingeben: "))
         if eingabe >= 2:
             break
 
-    prim_liste = rkmath.sieb_eratosthenes(100)
-
-    for i, val in enumerate(prim_liste):
-        if prim_liste[i] == True:
-            while eingabe%i == 0:
-                eingabe = eingabe/i;
-                zer_liste.append(i)
-                
+    zer_liste = rkmath.get_primfaktorzerlegung(eingabe)
     print(zer_liste)
 
 if __name__ == '__main__':
